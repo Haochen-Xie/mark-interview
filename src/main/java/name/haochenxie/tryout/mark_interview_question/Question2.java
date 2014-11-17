a0da4947a831df7980abecbabb7a52cccfe7115f
Haochen Xie <haochen@xie.name>
2014-11-17 16:59:08 +0900

    Correct Question 2
    Signed-off-by: Haochen Xie <haochen@xie.name>

--- src/main/java/name/haochenxie/tryout/mark_interview_question/Question2.java
+++ src/main/java/name/haochenxie/tryout/mark_interview_question/Question2.java
@@ -3,7 +3,12 @@ package name.haochenxie.tryout.mark_interview_question;
 public class Question2 {
 	
 	public static String rbg2html(int red, int green, int blue) {
-		return "#" + Integer.toHexString(red) + Integer.toHexString(green) + Integer.toHexString(blue);
+		return "#" + int2hex(red) + int2hex(green) + int2hex(blue);
+	}
+	
+	public static String int2hex(int num) {
+		String str = Integer.toHexString(num);
+		return str.length() == 2 ? str : "0" + str;
 	}
 	
 }
