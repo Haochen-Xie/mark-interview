dd088177fbaa2ffd3c19fae1b452d158cd41cf70
Haochen Xie <haochen@xie.name>
2014-11-17 15:33:40 +0900

    Implement Question2#rbg2html
    Signed-off-by: Haochen Xie <haochen@xie.name>

---
+++ src/main/java/name/haochenxie/tryout/mark_interview_question/Question2.java
@@ -0,0 +1,9 @@
+package name.haochenxie.tryout.mark_interview_question;
+
+public class Question2 {
+	
+	public static String rbg2html(int red, int green, int blue) {
+		return "#" + Integer.toHexString(red) + Integer.toHexString(green) + Integer.toHexString(blue);
+	}
+	
+}
